df267cfdf9156cd5112f553242b00dc29008bc18
Reisen <reisen@morphism.org>
2022-07-05 13:15:07 +0000

    sdk/rust: update chains

--- sdk/rust/core/src/chain.rs
+++ sdk/rust/core/src/chain.rs
@@ -3,40 +3,62 @@
 use std::convert::TryFrom; // Remove in 2021
 
 
-/// Chain contains a mapping of Wormhole supported chains to their u16 representation. These are
+/// Chain is a mapping of Wormhole supported chains to their u16 representation. These are
 /// universally defined among all Wormhole contracts.
 #[repr(u16)]
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Chain {
-    All      = 0,
-    Solana   = 1,
-    Ethereum = 2,
-    Terra    = 3,
-    Binance  = 4,
-    Polygon  = 5,
-    AVAX     = 6,
-    Oasis    = 7,
+    Any          = 0,
+    Solana       = 1,
+    Ethereum     = 2,
+    TerraClassic = 3,
+    Binance      = 4,
+    Polygon      = 5,
+    Avalanche    = 6,
+    Oasis        = 7,
+    Algorand     = 8,
+    Aurora       = 9,
+    Fantom       = 10,
+    Karura       = 11,
+    Acala        = 12,
+    Klaytn       = 13,
+    Celo         = 14,
+    Near         = 15,
+    Terra        = 18,
 }
 
 impl TryFrom<u16> for Chain {
     type Error = ();
+
+    #[rustfmt::skip]
     fn try_from(other: u16) -> Result<Chain, Self::Error> {
+        use Chain::*;
+
         match other {
-            0 => Ok(Chain::All),
-            1 => Ok(Chain::Solana),
-            2 => Ok(Chain::Ethereum),
-            3 => Ok(Chain::Terra),
-            4 => Ok(Chain::Binance),
-            5 => Ok(Chain::Polygon),
-            6 => Ok(Chain::AVAX),
-            7 => Ok(Chain::Oasis),
-            _ => Err(()),
+            0  => Ok(Any),
+            1  => Ok(Solana),
+            2  => Ok(Ethereum),
+            3  => Ok(TerraClassic),
+            4  => Ok(Binance),
+            5  => Ok(Polygon),
+            6  => Ok(Avalanche),
+            7  => Ok(Oasis),
+            8  => Ok(Algorand),
+            9  => Ok(Aurora),
+            10 => Ok(Fantom),
+            11 => Ok(Karura),
+            12 => Ok(Acala),
+            13 => Ok(Klaytn),
+            14 => Ok(Celo),
+            15 => Ok(Near),
+            18 => Ok(Terra),
+            _  => Err(()),
         }
     }
 }
 
 impl Default for Chain {
     fn default() -> Self {
-        Self::All
+        Self::Any
     }
 }
